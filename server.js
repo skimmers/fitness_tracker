97df001eefc42b636050082976861684c06ff0f7
Sharon Kim <skimilk19@gmail.com>
2021-05-05 15:08:09 -0500

    server.js complete

--- server.js
+++ server.js
@@ -2,3 +2,29 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+
+//set port
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+//use logger
+app.use(logger("dev"));
+
+//parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//use static files
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost/fittracker", {useNewUrlParser: true});
+
+
+//use routes
+require('./routes/api-routes')(app)
+require('./routes/html-routes')(app)
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}..`);
+})
